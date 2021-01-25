6c2cef09043cbc5c0bc81ce1cb4632a0b8c0c8d3
jotabraham <jotabraham@gmail.com>
2021-01-25 16:44:27 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const fullName = "John Abraham";
+let age = 29;
+const birthday = "December 11";
+const detroitGC = false;
+let lifeEvents = [
+  "I was born in Detroit, Michigan",
+  "I toured China with a skateboard team in 2013",
+  "I graduated from Kuyper college",
+  "My wife and I got married in Glacier Nat. Park in 2018",
+];
+if (detroitGC) {
+  console.log(``);
+}
